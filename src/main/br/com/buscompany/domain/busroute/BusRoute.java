eb7a9c6a5517283d5b253479bf3c9dfb3d086c3e
Vinicius de Paula <viniciusilvav1@hotmail.com>
2022-04-12 17:12:26 -0300

    add movement in BusRoute class

--- src/main/br/com/buscompany/domain/busroute/BusRoute.java
+++ src/main/br/com/buscompany/domain/busroute/BusRoute.java
@@ -1,7 +1,6 @@
 package main.br.com.buscompany.domain.busroute;
 
-import main.br.com.buscompany.domain.bus.Bus;
-
+import java.security.InvalidParameterException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -9,8 +8,75 @@ public class BusRoute {
 
     private List<BusStop> route = new LinkedList<>();
 
+    private Integer lastBusStop = 0;
+
+    private Integer nextBusStop = 0;
+
     public void addStop(BusStop busStop) {
         this.route.add(busStop);
+    }
+
+    public List<BusStop> showRoute() {
+        return this.route;
+    }
+
+    public void move() {
+        this.setLastBusStop(this.nextBusStop);
+        this.setNextBusStop(this.nextBusStop + 1);
+    }
+
+    private void setLastBusStop(Integer lastBusStop) {
+        this.verifyStopIsNull(lastBusStop);
+
+        this.verifyIfBusStopNotIsLessZero(lastBusStop);
+
+        this.verifyIfStopNotIsMoreOfRouteSize(lastBusStop);
+
+        this.verifyIfStopNotIsMoreOfNextStop(lastBusStop);
+
+        this.lastBusStop = lastBusStop;
+    }
+
+    private void setNextBusStop(Integer nextBusStop) {
+        this.verifyStopIsNull(nextBusStop);
+
+        this.verifyIfStopNotIsMoreOfRouteSize(nextBusStop);
+
+        this.verifyIfBusStopNotIsLessZero(nextBusStop);
+
+        this.verifyIfStopIsLessLastBusStop(nextBusStop);
+
+        this.nextBusStop = nextBusStop;
+    }
+
+    private void verifyIfStopIsLessLastBusStop(Integer nextBusStop) {
+        if(nextBusStop < this.lastBusStop) {
+            throw new InvalidParameterException("Nao é possivel retornar uma parada");
+        }
+    }
+
+    private void verifyIfStopNotIsMoreOfNextStop(Integer lastBusStop) {
+        if(lastBusStop > this.nextBusStop) {
+            throw new InvalidParameterException("A ultima para nao pode ser maior que a proxima");
+        }
+    }
+
+    private void verifyIfBusStopNotIsLessZero(Integer nextBusStop) {
+        if(nextBusStop < 0) {
+            throw new InvalidParameterException("Proxima para da e menor que tamanho minimo da rota");
+        }
+    }
+
+    private void verifyIfStopNotIsMoreOfRouteSize(Integer nextBusStop) {
+        if(nextBusStop > this.route.size()) {
+            throw new InvalidParameterException("Proxima parada e maior que o tamanho da rota");
+        }
+    }
+
+    private void verifyStopIsNull(Integer nextBusStop) {
+        if(nextBusStop == null) {
+            throw new InvalidParameterException("Proxima parada nao deve ser nulo");
+        }
     }
 
 }
